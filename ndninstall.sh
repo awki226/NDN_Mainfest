1986bfb4338268347eca215a31896107030d5f5e
Alex W King <70342734+awki226@users.noreply.github.com>
2023-03-27 14:51:26 -0400

    Update ndninstall.sh

--- ndninstall.sh
+++ ndninstall.sh
@@ -5,11 +5,11 @@ sudo yum install -y doxygen graphviz python3-pip
 pip3 install -y --user sphinx sphinxcontrib-doxylink
 git clone https://github.com/named-data/ndn-cxx.git
 cd ndn-cxx
+export LD_LIBRARY_PATH='/usr/local/lib64'
 #May have to use devtoolset-8 for centos 7
 ./waf configure --without-pch
 ./waf
 sudo ./waf install
-export LD_LIBRARY_PATH='/usr/local/lib64'
 sudo ldconfig
 cd ..
 git clone --recursive https://github.com/named-data/NFD.git
@@ -18,7 +18,6 @@ cd NFD
 ./waf configure --without-pch
 ./waf 
 sudo ./waf install
-export LD_LIBRARY_PATH='/usr/local/lib64'
 sudo ldconfig
 sudo cp /usr/local/etc/ndn/nfd.conf.sample /usr/local/etc/ndn/nfd.conf
 sudo vi /usr/local/etc/ndn/nfd.conf
